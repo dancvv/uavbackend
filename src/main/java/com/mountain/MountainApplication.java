f116a7f5cdd2da83c4b3870f1cd8625b8ec46d12
Black <uquantum@hotmail.com>
2021-05-14 19:07:15 +0800

    first page

---
+++ src/main/java/com/mountain/MountainApplication.java
@@ -0,0 +1,13 @@
+package com.mountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainApplication.class, args);
+    }
+
+}
