4081655164d125d3e7421a62864b268219892cf2
Black <uquantum@hotmail.com>
2021-05-25 20:00:15 +0800

    DAO层修改

--- src/main/java/com/mountain/controller/BusController.java
+++ src/main/java/com/mountain/controller/BusController.java
@@ -1,9 +1,11 @@
 package com.mountain.controller;
 
 import com.mountain.DAO.BusDAO;
+import com.mountain.DAO.RouteDAO;
 import com.mountain.entity.Bus;
 import com.mountain.entity.Location;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,11 @@ import java.util.Collection;
 @RestController
 @Api(tags={"公交车信息"})
 public class BusController {
+    @Autowired
+    BusDAO busDAO;
+//    @Autowired
+//    RouteDAO routeDAO;
+    BusDAO bs = new BusDAO();
     @GetMapping("/getbus")
     public Bus GetBus(){
         Bus setBus=new Bus();
@@ -24,7 +31,8 @@ public class BusController {
         return setBus;
     }
     @GetMapping("/businfo")
-    public String busInfo(){
-        Collection<Bus> busInform= BusDAO.get
+    public Collection<Bus> busInfo(){
+        Collection<Bus> busInform = busDAO.getAll();
+        return busInform;
     }
 }
