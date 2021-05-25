fa01cd123c6029ebf43032711f1cde52a79bae17
Black <uquantum@hotmail.com>
2021-05-25 18:47:01 +0800

    DAO层更新

--- src/main/java/com/mountain/controller/BusController.java
+++ src/main/java/com/mountain/controller/BusController.java
@@ -1,5 +1,6 @@
 package com.mountain.controller;
 
+import com.mountain.DAO.BusDAO;
 import com.mountain.entity.Bus;
 import com.mountain.entity.Location;
 import io.swagger.annotations.Api;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @Api(tags={"公交车信息"})
@@ -19,5 +22,9 @@ public class BusController {
     @PostMapping("/postbus")
     public Bus PostBus(@RequestBody Bus setBus){
         return setBus;
+    }
+    @GetMapping("/businfo")
+    public String busInfo(){
+        Collection<Bus> busInform= BusDAO.get
     }
 }
