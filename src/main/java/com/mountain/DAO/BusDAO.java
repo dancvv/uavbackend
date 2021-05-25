0e183b5a617785f51c3a62df95d1d0d564351f83
blackMountain <wangvei@mail.dlut.edu.cn>
2021-05-25 23:43:36 +0800

    DAO层测试通过

--- src/main/java/com/mountain/DAO/BusDAO.java
+++ src/main/java/com/mountain/DAO/BusDAO.java
@@ -6,12 +6,14 @@ import com.mountain.entity.Stations;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 @Repository
 public class BusDAO {
-    private static Map<Integer, Bus> busSet=null;
+    private static Map<Integer, Bus> busSet;
     static {
+        busSet=new HashMap<Integer,Bus>();
         busSet.put(2201,new Bus(null,2201,new Location("10717","131.027809"),11,15,21,21,new Stations(10,"1无名站",new Location("121.50717","31.027809")),new Stations(21,"1秋明站",new Location("11.50717","131.027809"))));
         busSet.put(2202,new Bus(null,2202,new Location("20717","231.027809"),12,25,22,22,new Stations(20,"2无名站",new Location("221.50717","32.027809")),new Stations(22,"2秋明站",new Location("21.50717","231.027809"))));
         busSet.put(2203,new Bus(null,2203,new Location("30717","331.027809"),13,35,23,23,new Stations(30,"3无名站",new Location("321.50717","33.027809")),new Stations(23,"3秋明站",new Location("31.50717","331.027809"))));
