f2ffc9dda06197e788419a2d01a609c0bb0b10b0
MountainWang <uquantum@hotmail.com>
2021-07-06 13:15:07 +0800

    update

--- src/main/java/com/mountain/entity/Order.java
+++ src/main/java/com/mountain/entity/Order.java
@@ -15,10 +15,10 @@ public class Order {
     private String passenger_id;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date date;
-    @JsonFormat(pattern = "HH:mm:ss")
-    private Date create_time;
-    @JsonFormat(pattern = "HH:mm:ss")
-    private Date check_ticket_time;
+//    @JsonFormat(pattern = "HH:mm:ss")
+    private String create_time;
+//    @JsonFormat(pattern = "HH:mm:ss")
+    private String check_ticket_time;
     private int up_stop_id;
     private int down_stop_id;
     private int ride_count;
