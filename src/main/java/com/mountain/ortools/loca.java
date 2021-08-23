e528a07322b8b55495a2ab0d9ab97d6a639291b1
laptopdot <uquantum@hotmail.com>
2021-08-24 02:12:03 +0800

    vrp问题

---
+++ src/main/java/com/mountain/ortools/loca.java
@@ -0,0 +1,13 @@
+package com.mountain.ortools;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class loca {
+    private Double lng;
+    private Double lat;
+}
