f7572c302759bd5466140e3a743ffe2cfe1d2f64
MountainWang <uquantum@hotmail.com>
2021-08-27 21:25:23 +0800

    WebSocket调试成功

--- src/main/java/com/mountain/Socket/WsServerEndpoint.java
+++ src/main/java/com/mountain/Socket/WsServerEndpoint.java
@@ -1,5 +1,6 @@
 package com.mountain.Socket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.OnClose;
@@ -10,13 +11,16 @@ import javax.websocket.server.ServerEndpoint;
 //暴露出去的应用路径
 @ServerEndpoint("/myws")
 @Component
+@Slf4j
 public class WsServerEndpoint {
     @OnOpen
     public void Open(Session session){
+        log.info("有新的客户端连接了");
         System.out.println("连接成功");
     }
     @OnClose
     public void onClose(Session session){
+        log.info("有用户断开了");
         System.out.println("连接关闭");
     }
     @OnMessage
--- src/main/java/com/mountain/config/WebSocketConfig.java
+++ src/main/java/com/mountain/config/WebSocketConfig.java
@@ -1,22 +1,15 @@
 package com.mountain.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+public class WebSocketConfig {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").withSockJS();
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
     }
 }
