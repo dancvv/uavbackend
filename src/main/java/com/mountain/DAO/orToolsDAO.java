baeb3d4f1b17e1abf29ba23bfb3abb4078e2787c
MountainWang <uquantum@hotmail.com>
2021-09-05 11:20:34 +0800

    距离计算

--- src/main/java/com/mountain/DAO/orToolsDAO.java
+++ src/main/java/com/mountain/DAO/orToolsDAO.java
@@ -14,6 +14,11 @@ public class orToolsDAO {
 //                    不是两两相减，而是与第一个元素相减
 //                    distanceMatrix[rowIndex][colIndex]=locations[colIndex]-locations[rowIndex];
 //                    获取两个数的平方和，即欧几里得距离
+                    double radiansAX=Math.toRadians(locations[colIndex][0]);//A点经度
+                    double radiansAY=Math.toRadians(locations[colIndex][1]);//A点维度
+                    double radiansBX=Math.toRadians(locations[rowIndex][0]);//B点经度
+                    double radiansBY=Math.toRadians(locations[rowIndex][1]);//B点维度
+
                     distanceMatrix[rowIndex][colIndex]=Math.hypot(locations[colIndex][0]-locations[rowIndex][0],locations[colIndex][1]-locations[rowIndex][1]);
                 }
 
