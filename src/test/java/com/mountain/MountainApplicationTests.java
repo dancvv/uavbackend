2c4905cab2609bf0f4df755c3702403b47b54e87
laptopdot <uquantum@hotmail.com>
2021-10-04 14:27:48 +0800

    测试问题

--- src/test/java/com/mountain/MountainApplicationTests.java
+++ src/test/java/com/mountain/MountainApplicationTests.java
@@ -1,13 +1,17 @@
 package com.mountain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MountainApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("测试通过");
     }
 
 }
