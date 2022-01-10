c9218cbb80362c86b45810a561c5dcd23861f4d1
server <uquantum@hotmail.com>
2022-01-10 14:15:23 +0000

    解决MongoDB数据源无法查询的问题，不是预期所想的代码逻辑问题

--- src/main/java/com/mountain/controller/TaxiUserController.java
+++ src/main/java/com/mountain/controller/TaxiUserController.java
@@ -1,14 +1,27 @@
 package com.mountain.controller;
 
+import java.util.List;
+
+import com.mountain.entity.TaxiUsers;
+import com.mountain.service.impl.TaxiUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/taxiuser")
 public class TaxiUserController {
-    @GetMapping("getLocation")
-    public void getLocation() {
-        
+
+    @Autowired
+    private TaxiUserServiceImpl taxiUserService;
+
+    @GetMapping("/getLocation")
+    public List<TaxiUsers> getLocation() {
+        List<TaxiUsers> allLocations = taxiUserService.getAllLocations();
+        // System.out.println(allLocations);
+        return allLocations;
     }
 }
--- src/main/java/com/mountain/entity/TaxiUsers.java
+++ src/main/java/com/mountain/entity/TaxiUsers.java
@@ -3,12 +3,17 @@ package com.mountain.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TaxiOnUsers {
-    private String ID;
+@Document(collection = "TaxiUsers")
+public class TaxiUsers {
+    @Id
+    private String id;
+    private String UserId;
     private int RiggerEvent;
     private int RunStatus;
     private String GPSTime;
